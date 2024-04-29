e101080be215eab39707bafc09ca2d06e5d3acc3
tebibux <tebibusolomon79@gmail.com>
2024-04-29 13:33:09 +0300

    component page structure done

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,6 +1,13 @@
+import ProductImage from "./subComponents/ProductImage"
+import ProductItems from "./subComponents/ProductItems"
+
 const ProductList = () => {
     return (
+        <>
         <h2>This is product list page</h2>
+        <ProductImage />
+        <ProductItems />
+        </>
     )
 }
 
---
+++ src/Components/subComponents/ProductItems.jsx
@@ -0,0 +1,6 @@
+const ProductItems = () => {
+  return (
+    <div>ProductItems</div>
+  )
+}
+export default ProductItems
